3b911c57fb2c750f0cb62b14135973bfd770f69c
saklainm71 <saklainm71@gmail.com>
2023-10-12 16:14:39 +0600

    peer to peer connection set up by flutter

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,279 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:sdp_transform/sdp_transform.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _localVideoRenderer = RTCVideoRenderer();
+  final _remoteVideoRenderer = RTCVideoRenderer();
+  final sdpController = TextEditingController();
+  bool _offer = false;
+
+
+  RTCPeerConnection? _peerConnection;
+  MediaStream? _localStream;
+
+  initRenderer() async {
+    await _localVideoRenderer.initialize();
+    await _remoteVideoRenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': true,
+      // 'video': {
+      //   'facingMode': 'user',
+      // }
+    };
+
+    MediaStream stream =
+    await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+    _localVideoRenderer.srcObject = stream;
+    return stream;
+  }
+
+  _createPeerConnecion() async {
+    Map<String, dynamic> configuration = {
+      "iceServers": [
+        {"url": "stun:stun.l.google.com:19302"},
+      ]
+    };
+
+    final Map<String, dynamic> offerSdpConstraints = {
+      "mandatory": {
+        "OfferToReceiveAudio": true,
+        "OfferToReceiveVideo": true,
+      },
+      "optional": [],
+    };
+
+    _localStream = await _getUserMedia();
+
+    RTCPeerConnection pc =
+    await createPeerConnection(configuration, offerSdpConstraints);
+
+    pc.addStream(_localStream!);
+
+    pc.onIceCandidate = (e) {
+      if (e.candidate != null) {
+        print(json.encode({
+          'candidate': e.candidate.toString(),
+          'sdpMid': e.sdpMid.toString(),
+          'sdpMlineIndex': e.sdpMLineIndex,
+        }));
+      }
+    };
+
+    pc.onIceConnectionState = (e) {
+      print(e);
+    };
+
+    pc.onAddStream = (stream) {
+      print('addStream: ' + stream.id);
+      _remoteVideoRenderer.srcObject = stream;
+    };
+
+    return pc;
+  }
+
+
+  void _createAudioOffer() async {
+    RTCSessionDescription description =
+    await _peerConnection!.createOffer({'offerToReceiveAudio': 1});
+    var session = parse(description.sdp.toString());
+    //print('createAudioOffer $session');
+    print(json.encode(session));
+    _offer = true;
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _createAudioAnswer() async {
+    RTCSessionDescription description =
+    await _peerConnection!.createAnswer({'offerToReceiveAudio': 1});
+
+    var session = parse(description.sdp.toString());
+    //print('createAudioAnswer $session');
+    print(json.encode(session));
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+
+  void _createOffer() async {
+    RTCSessionDescription description =
+    await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
+    var session = parse(description.sdp.toString());
+    //print('createOffer $session');
+    print(json.encode(session));
+    _offer = true;
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _createAnswer() async {
+    RTCSessionDescription description =
+    await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
+
+    var session = parse(description.sdp.toString());
+    //print('createOffer $session');
+    print(json.encode(session));
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _setRemoteDescription() async {
+    String jsonString = sdpController.text;
+    dynamic session = await jsonDecode(jsonString);
+    //print('setRemoteDescription $session');
+
+    String sdp = write(session, null);
+
+    RTCSessionDescription description =
+    RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
+    print(description.toMap());
+
+    await _peerConnection!.setRemoteDescription(description);
+  }
+
+  void _addCandidate() async {
+    String jsonString = sdpController.text;
+    dynamic session = await jsonDecode(jsonString);
+    //print('addCandidate $session');
+    print(session['candidate']);
+    dynamic candidate = RTCIceCandidate(
+        session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
+     await _peerConnection!.addCandidate(candidate);
+  }
+
+
+  @override
+  void initState() {
+    initRenderer();
+    _createPeerConnecion().then((pc) {
+      _peerConnection = pc;
+    });
+    // _getUserMedia();
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    await _localVideoRenderer.dispose();
+    await _remoteVideoRenderer.dispose();
+    _peerConnection!.close();
+    sdpController.dispose();
+    super.dispose();
+  }
+
+  SizedBox videoRenderers() => SizedBox(
+    height: 210,
+    child: Row(children: [
+      Flexible(
+        child: Container(
+          key: const Key('local'),
+          margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+          decoration: const BoxDecoration(color: Colors.black),
+          child: RTCVideoView(_localVideoRenderer),
+        ),
+      ),
+      Flexible(
+        child: Container(
+          key: const Key('remote'),
+          margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+          decoration: const BoxDecoration(color: Colors.black),
+          child: RTCVideoView(_remoteVideoRenderer),
+        ),
+      ),
+    ]),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            videoRenderers(),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: TextField(
+                      controller: sdpController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 4,
+                      maxLength: TextField.noMaxLength,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: _createOffer,
+                      child: const Text("Offer"),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: _createAnswer,
+                      child: const Text("Answer"),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: _setRemoteDescription,
+                      child: const Text("Set Remote Description"),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: _addCandidate,
+                      child: const Text("Set Candidate"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async{
+                        // Assuming _localVideoRenderer is an RTCVideoRenderer
+                        await _localVideoRenderer.dispose();
+                        await _localVideoRenderer.initialize();
+                      },
+                      child: const Text("Refresh local"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async{
+                        // Assuming _localVideoRenderer is an RTCVideoRenderer
+                        await _remoteVideoRenderer.dispose();
+                        await _remoteVideoRenderer.initialize();
+                      },
+                      child: const Text("Refresh Remote"),
+                    ),
+
+                  ],
+                )
+              ],
+            ),
+          ],
+        ));
+  }
+}
